189949ae007534e8ac02320bb09cd4c693134da6
shoce <yogze@icloud.com>
2024-09-06 01:10:38 +0530

    regexp

--- parser.go
+++ parser.go
@@ -22,17 +22,12 @@ type failedConnEventParser struct{}
 
 var (
 	errWrongFormat = errors.New("wrong event format")
-	eRegex         = regexp.MustCompile(`^(?<ts>[-0-9T]+).*: Invalid user (?<user>\w+) from (?<addr>[^ ]+) port (?<port>[^ ]+)`)
+	eRegex         = regexp.MustCompile(`^(?<ts>[-0-9T]+).*: Invalid user (?<username>\w+) from (?<ipaddr>[^ ]+) port (?<tcpport>[^ ]+)`)
 )
 
 func (p failedConnEventParser) Parse(s string) (FailedConnEvent, error) {
 	rs := eRegex.FindStringSubmatch(s)
-	if len(rs) != 6 {
-		return FailedConnEvent{}, errWrongFormat
-	}
-
-	portNumber, err := strconv.Atoi(rs[4])
-	if err != nil {
+	if len(rs) != 5 {
 		return FailedConnEvent{}, errWrongFormat
 	}
 
@@ -41,13 +36,21 @@ func (p failedConnEventParser) Parse(s string) (FailedConnEvent, error) {
 		return FailedConnEvent{}, errWrongFormat
 	}
 
+	username := rs[2]
+	ipaddr := net.ParseIP(rs[3])
+
+	tcpport, err := strconv.Atoi(rs[4])
+	if err != nil {
+		return FailedConnEvent{}, errWrongFormat
+	}
+
 	// The logs do not have information about the year, so we're just assuming we're parsing current year logs
-	ts = time.Date(time.Now().Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), ts.Nanosecond(), time.UTC)
+	ts = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), ts.Nanosecond(), time.UTC)
 
 	return FailedConnEvent{
-		Username:  rs[2],
-		IPAddress: net.ParseIP(rs[3]),
-		Port:      portNumber,
+		Username:  username,
+		IPAddress: ipaddr,
+		Port:      tcpport,
 		Timestamp: ts,
 		Country:   "unknown",
 	}, nil
